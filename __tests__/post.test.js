777c72274fdaeae006ebb5211d10531330652334
satyadvaya <satyadvaya@gmail.com>
2021-10-18 10:57:38 -0700

    failing test GET post by id

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -3,6 +3,7 @@ const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
 const User = require('../lib/models/User');
+const Post = require('../lib/models/Post');
 
 jest.mock('../lib/middleware/ensureAuth.js', () => {
   return (req, res, next) => {
@@ -31,6 +32,30 @@ describe('faceSpace /posts routes', () => {
       media: 'media.gif',
       notifications: false,
     });
+
+    expect(res.body).toEqual({
+      id: '1',
+      username: user.username,
+      notifications: false,
+      text: 'text-here',
+      media: 'media.gif',
+    });
+  });
+
+  it('should get a post by id', async () => {
+    const user = await User.insert({
+      username: 'test-user',
+      avatar: 'image.png',
+    });
+
+    const post = await Post.insert({
+      username: user.username,
+      notifications: false,
+      text: 'text-here',
+      media: 'media.gif',
+    });
+
+    const res = await request(app).get('/posts/1');
 
     expect(res.body).toEqual({
       id: '1',

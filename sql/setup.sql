ac2eda4b1fb80d47e62fcf6ad67d7bb4d6cc9bf2
Joseph-K-B <303josephb@gmail.com>
2021-10-19 13:15:43 -0700

    added SQL tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,9 @@
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS posts CASCADE;
-DROP TABLE IF EXISTS likes;
 DROP TABLE IF EXISTS comments CASCADE;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS listings;
+
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -13,26 +15,37 @@ CREATE TABLE users (
 CREATE TABLE posts (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id BIGINT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
     notifications BOOLEAN DEFAULT false,
     text TEXT,
-    media_url TEXT NOT NULL
+    media_url TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
--- CREATE TABLE comments (
---     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---     username TEXT NOT NULL,
---     comment TEXT NOT NULL,
---     post_id BIGINT NOT NULL,
---     FOREIGN KEY(username) REFERENCES users(google_username),
---     FOREIGN KEY(post_id) REFERENCES posts(id)
--- );
+CREATE TABLE comments (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    comment TEXT NOT NULL,
+    post_id BIGINT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(post_id) REFERENCES posts(id)
+);
 
--- CREATE TABLE likes (
---     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---     post_id BIGINT NOT NULL,
---     username TEXT NOT NULL,
---     FOREIGN KEY(post_id) REFERENCES posts(id),
---     FOREIGN KEY(username) REFERENCES users(google_username)
--- );
-INSERT INTO users (google_username, google_email, google_avatar_url) VALUES ('joe mac', 'joe.email.com', 'http://local.com');
+CREATE TABLE likes (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    post_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    FOREIGN KEY(post_id) REFERENCES posts(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE listings (
+    id BIGINT GENERATED ALWAYS AS PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    description TEXT,
+    price MONEY NOT NULL,
+    photo TEXT NOT NULL,
+    -- category_id BIGINT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    -- FOREIGN KEY(category_id) REFERENCES categories(id)
+)
+

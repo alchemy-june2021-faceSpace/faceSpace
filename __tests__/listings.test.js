41d1583055a7b9e7cc5389d3101b3ea65d16d804
TANNER MECK <steve.meck246@gmail.com>
2021-10-19 19:35:37 -0700

    delete route for listing setup and failing

--- __tests__/listings.test.js
+++ __tests__/listings.test.js
@@ -100,9 +100,9 @@ describe('faceSpace routes', () => {
     });
   });
 
-  it.only('should return unauthorized when a user attempts to update a item that they did not post', async () => {
+
+  it('deletes listing but it\'s id', async () => {
     await User.insert(standardUser);
-    
     await request(app)
       .post('/listings')
       .send({
@@ -110,39 +110,17 @@ describe('faceSpace routes', () => {
         price: '$1.50',
         photo: 'image.png'
       });
-    await User.insert({
-      username: 'test-user2',
-      email: 'test2-email@email2.com',
-      avatar: 'image2.png',
-    });
-    await pool.query(
-      'INSERT INTO listings (user_id, description, price, photo) VALUES ($1, $2, $3, $4)',
-      ['2', 'user 2 placed item', '$1.99', 'photo.com']
-    );
-    
     const res = await request(app)
-      .put('/listings/2')
-      .send({
-        description: 'Great Item',
-        price: '$14.50',
-        photo: 'www.image.png'
-      });
+      .delete('/listings/1');
 
     expect(res.body).toEqual({
       id: expect.any(String),
-      userId: expect.any(String),
-      description: 'Great Item',
-      price: '$14.50',
-      photo: 'www.image.png'
+      user_id: expect.any(String),
+      description: 'GREAT BUY',
+      price: '$1.50',
+      photo: 'image.png'
     });
   });
-
-  // it('deletes listing but it\'s id', async () => {
-  //   const res = await request(app)
-  //     .delete('/listings/5');
-
-  //   expect(res.body).toEqual({});
-  // });
 
   afterAll(() => {
     pool.end();

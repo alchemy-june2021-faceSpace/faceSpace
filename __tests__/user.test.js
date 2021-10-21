57a2f4c471370783f17e45d8f65d979144b77dbf
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2021-10-20 17:35:53 -0700

    put for users passing

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -26,6 +26,24 @@ describe('faceSpace /comments routes', () => {
     return setup(pool);
   });
 
+  it('updates a user by their id', async () => {
+    await User.insert(standardUser);
+
+    const res = await request(app)
+      .put('/user/1')
+      .send({
+        username: 'test-user',
+        avatar: 'image-2.png',
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      username: 'test-user',
+      email: 'blah@blah.com',
+      avatar: 'image-2.png',
+    });
+  });
+
   it('should delete the users profile returning the deleted user', async () => {
     await User.insert(standardUser);
 
@@ -33,6 +51,7 @@ describe('faceSpace /comments routes', () => {
       .delete('/user/1');
 
     expect(res.body).toEqual({
+      id: '1',
       username: 'test-user',
       email: 'blah@blah.com',
       avatar: 'image.png',
